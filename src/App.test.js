0e78801b3b541e181d291a9569ca070ca3a67ace
TrevorRezac <trev.rezac@gmail.com>
2022-02-10 11:04:52 -0800

    passing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ListPage from './ListPage';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('List Page', async () => {
+  render(<ListPage />);
+  const linkElement = await screen.findByText(/List Page/i);
   expect(linkElement).toBeInTheDocument();
 });
